6782b557dd038323f4f863f6d327a4dfd6635f33
Ivan Gorokhov <xgorokhovx@gmail.com>
2025-02-24 01:44:47 +0200

    feat: add tests

--- src/tests/components/UI/OuterWrapper.test.tsx
+++ src/tests/components/UI/OuterWrapper.test.tsx
@@ -1,18 +1,25 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
-import { vi, describe, it, expect } from 'vitest';
+import { vi, describe, it, expect, beforeEach } from 'vitest';
 import OutletWrapper from '../../../components/UI/OutletWrapper.tsx';
 
+// Mock useNavigate
+const mockNavigate = vi.fn();
+
 vi.mock('react-router-dom', async () => {
-  const actual = await vi.importActual('react-router');
+  const actual = await vi.importActual('react-router-dom');
   return {
     ...actual,
-    useNavigate: vi.fn(),
+    useNavigate: () => mockNavigate,
   };
 });
 
 describe('OutletWrapper', () => {
-  it('renders the Outlet component', () => {
+  beforeEach(() => {
+    mockNavigate.mockClear(); // Reset mock function before each test
+  });
+
+  it('renders the Outlet component when not on the root path', () => {
     render(
       <MemoryRouter initialEntries={['/home']}>
         <Routes>
@@ -24,5 +31,20 @@ describe('OutletWrapper', () => {
     );
 
     expect(screen.getByText('Home Page')).toBeInTheDocument();
+    expect(mockNavigate).not.toHaveBeenCalled();
+  });
+
+  it('redirects to /home when on the root path', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<OutletWrapper />}>
+            <Route path="home" element={<div>Home Page</div>} />
+          </Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    expect(mockNavigate).toHaveBeenCalledWith('/home');
   });
 });
